b5fa6dabee1dd918f5c9af833e651199e13007ac
vigneshvikki23 <vigneshvikkiqwert@gmail.com>
2022-09-08 20:11:19 -0600

    waran's code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -1,5 +1,11 @@
 package org.test;
 
 public class Sample {
+	public static void main(String[] args) {
+		System.out.println("test 1");
+		System.out.println("test 4");
+		System.out.println("test 5");
+		
+	}
 
 }
